4c3e7a363b08e550b9b0b46d0af2eb481530ef43
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-02-05 07:53:39 +0300

    for

---
+++ app.js
@@ -0,0 +1,39 @@
+// let n = 5;
+// let counter = 0;
+
+// for (i = 0; i <= n; i++) {
+//   console.log(`(${n} + ${i}) ** 2 = ${(n + i) ** 2}`);
+//   counter += (n + i) ** 2;
+// }
+// console.log("sum :", counter);
+
+// let son = 5;
+// let power = son;
+// let sum = 0;
+
+// for (i = 1; i <= son; i++) {
+//   sum += i ** power;
+//   power--;
+// }
+
+// console.log(sum);
+
+// i nechi bosa k  shuncha bop chqadi formula
+// let sum = "";
+// for (let i = 1; i <= 5; i++) {
+//   for (k = 1; k <= i; k++) {
+//     sum += `${i}`;
+//   }
+//   console.log(sum.trimEnd());
+//   sum = "";
+// }
+
+// soni boluvchilarni topish formulasi masalan 10 niki 1 , 2, 5, 10
+// let n = 10;
+// let counter = "";
+// for (i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     counter += `${i} `;
+//   }
+// }
+// console.log(counter.trimEnd());
